8fc75e3bcf49f0cc2f5183a2f67cc8810dcbd35e
Abbaan Nassar <abbaan31@hotmail.com>
2024-01-25 00:01:24 +0100

    initial repo structure

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+# Read the contents of your requirements file
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='vigilant_dev',
+    version='0.1',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=required,
+    author='Abbaan Nassar',
+    author_email='abbaan31@hotmail.com',
+    description='A package for visualizing personal development using NLP and 3D visualization.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown'
+)
